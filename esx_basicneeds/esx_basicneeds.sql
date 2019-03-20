15db42965d6c7c727c98c9c78b99af00ee63a09e
David Carneiro <mrdeividcg@gmail.com>
2019-03-20 19:36:40 +0000

    Added new drinks and props

--- esx_basicneeds/esx_basicneeds.sql
+++ esx_basicneeds/esx_basicneeds.sql
@@ -1,6 +1,24 @@
 USE `essentialmode`;
 
-INSERT INTO `items` (`name`, `label`, `limit`) VALUES
-	('bread', 'Pain', 10),
-	('water', 'Eau', 5)
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+	('absinthe', 'Absinto', 5, 0, 1),
+	('beer', 'Cerveja', 15, 0, 1),
+	('bread', 'Pão', 15, 0, 1),
+	('champagne', 'Champanhe', 10, 0, 1),
+	('chips', 'Batatas Fritas', 5, 0, 1),
+	('chocolate', 'Chocolate', 10, 0, 1),
+	('cigarett', 'Cigarro', 20, 0, 1),
+	('cocacola', 'Coca-cola', 15, 0, 1),
+	('coffe', 'Café', 15, 0, 1),
+	('cupcake', 'Bolinho', 15, 0, 1),
+	('gintonic', 'Gin Tónico', 5, 0, 1),
+	('hamburger', 'Hamburger', 15, 0, 1),
+	('icetea', 'Ice-tea', 15, 0, 1),
+	('lighter', 'Isqueiro', 1, 0, 1),
+	('milk', 'Leite', 15, 0, 1),
+	('sandwich', 'Sandwich', 15, 0, 1),
+	('tequila', 'Tequila', 15, 0, 1),
+	('vodka', 'Vodka', 15, 0, 1),
+	('water', 'Água', 15, 0, 1),
+	('whisky', 'Whisky', 15, 0, 1)
 ;
